f5cabd879e0cfd5bef03ea09cbddde17a9476754
compilelife <505610414@qq.com>
2020-05-07 22:01:01 +0800

    linux libFcitxQt5DBusAddons.so/libFcitxQt5WidgetsAddons.so

--- src/release_linux.sh
+++ src/release_linux.sh
@@ -7,8 +7,6 @@ cp loginsight appdir/usr/bin/
 cp ../src/img/logo.png appdir/
 cp ../src/loginsight.desktop appdir/
 mkdir appdir/usr/lib
-cp /usr/lib/x86_64-linux-gnu/libFcitxQt5DBusAddons.so* appdir/usr/lib/
-cp /usr/lib/x86_64-linux-gnu/libFcitxQt5WidgetsAddons.so* appdir/usr/lib/
 ../tool/linuxdeployqt appdir/usr/bin/loginsight -appimage
 mkdir artifact
 mv loginsight-*.AppImage artifact/loginsight.AppImage
