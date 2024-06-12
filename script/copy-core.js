3b36983060bf6744a8920fe819bfd07e6912dbcf
cy <cy>
2024-06-12 12:40:16 +0800

    支持Universal打包

--- script/copy-core.js
+++ script/copy-core.js
@@ -1,9 +1,21 @@
-const {rename} = require('node:fs/promises');
+const { rm, copyFile} = require('node:fs/promises');
+const {Arch} = require('electron-builder')
+const path = require('path');
 
 exports.default = async function(context) {
-  if (context.electronPlatformName !== 'darwin')
+  if ((context.electronPlatformName !== 'darwin' && context.electronPlatformName != 'mas') || context.arch === Arch.universal)
     return
 
-  console.log('mv core to MacOS')
-  await rename(context.appOutDir+'/loginsight.app/Contents/core.darwin', context.appOutDir+'/loginsight.app/Contents/MacOS/core.darwin')
+  const projectPath = path.resolve(__dirname, '..');
+  console.log('mv core to MacOS', context.arch)
+
+  const unusedPath = context.appOutDir+'/loginsight.app/Contents/core.darwin'
+  const targetPath = context.appOutDir+'/loginsight.app/Contents/MacOS/core.darwin'
+  let corePath = projectPath +"/core.darwin"
+  if (context.arch === Arch.x64) {
+    corePath += '.x64'
+  }
+  
+  await rm(unusedPath)
+  await copyFile(corePath, targetPath)
 }
